88140d0794347fae14c77037e5e88e4324e7293c
Krishna Singh <70067302+Krishna-coder6111@users.noreply.github.com>
2024-04-23 22:27:14 +0000

    a5 final

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,37 +1,45 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { Navigate, Route, Routes, useParams } from "react-router-dom";
 import { HiMiniBars3 } from "react-icons/hi2";
-import CourseNavigation from "../Courses/Navigation";
+import CourseNavigation from "./Navigation";
 import Modules from "./Modules";
 import Home from "./Home";
 import Assignments from "./Assignments";
-import AssignmentEditor from "./Assignments/Editor";
 
-import coursesData from "../../Kanbas/Database"; // Renamed to avoid conflict
-
-function Courses({courses}: {courses: any[];}) {
+function Courses({ courses }: { courses: any[]; }) {
   const { courseId } = useParams();
-  const course = courses.find(
-    (course => course._id === courseId)
-  );
+  const COURSES_API = "https://kanbas-node-server-app-778x.onrender.com/api/courses";
+  const [course, setCourse] = useState<any>({ _id: "" });
+  const findCourseById = async (courseId?: string) => {
+    const response = await axios.get(
+      `${COURSES_API}/${courseId}`
+    );
+    setCourse(response.data);
+  };
+  useEffect(() => {
+    findCourseById(courseId);
+  }, [courseId]);
+
   return (
     <div>
       <h1><HiMiniBars3 /> Course {course?.name}</h1>
       <CourseNavigation />
       <div>
-      <div
+        <div
           className="overflow-y-scroll position-fixed bottom-0 end-0"
           style={{ left: "320px", top: "50px" }} >
-
-        <Routes>
+          <Routes>
             <Route path="/" element={<Navigate to="Home" />} />
-            <Route path="Home" element={<Home />} />
-            <Route path="Modules" element={<Modules />} />
-            <Route path="Assignments" element={<Assignments />} />
-            <Route path="Assignments/:assignmentId" element={<AssignmentEditor />} />
+            <Route path="Home" element={<Home/>} />
+            <Route path="Modules" element={<Modules/>} />
+            <Route path="Piazza" element={<h1>Piazza</h1>} />
+            <Route path="Assignments" element={<Assignments/>} />
+            <Route path="Assignments/:assignmentId" element={<h1>Assignment Editor</h1>} />
             <Route path="Grades" element={<h1>Grades</h1>} />
-        </Routes>
+          </Routes>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
